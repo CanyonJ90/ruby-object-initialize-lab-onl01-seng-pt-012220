7d00e9d5c76de998a3928e5dde7bc3488ec169e1
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-07 23:03:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,16 @@
 class Person
-  def initalize(persons_name)
+  def initialize (persons_name)
     @name = persons_name
   end
-  
-  def name= (persons_name)
-    @name= persons_name
+
+  def name=(persons_name)
+    @name = persons_name
   end
+
   def name
     @name
   end
- end 
- 
+  
+end
 
  
