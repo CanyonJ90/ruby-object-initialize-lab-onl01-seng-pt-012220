850cf32848e11a3281c6dfdf87857be13153259d
Canyon Mathis <canyon.mathis@gmail.com>
2020-02-07 23:39:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -14,12 +14,12 @@ class Person
 end
 
 class Dog 
-  def initialize (dogs_name, breed = "mutt")
+  def initialize (dogs_name, dogs_breed = "mutt")
     @name = dogs_name
     @breed = dogs_breed
   end
 
-  def name= (dogs_name, breed = "mutt")
+  def name= (dogs_name, dogs_breed = "mutt")
     @name = dogs_name
     @breed = dogs_breed
   end
